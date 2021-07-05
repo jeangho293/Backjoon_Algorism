00a579c5f91d87b9127c8f9568b2a85c8baf44ee
jeang <jeangho293@naver.com>
2021-07-05 19:10:00 +0900

    오등큰수 주석 추가

--- 골드문제/오등큰수/오등큰수.py
+++ 골드문제/오등큰수/오등큰수.py
@@ -1,3 +1,6 @@
+# 오큰수 문제에서 '비교 대상이 등장횟수'로 바뀐 것뿐이다.
+# 즉 Counter 를 활용해 등장횟수를 해쉬로 저장하여 불러오는 작업만 추가될뿐 크게 달라지는 로직은 전혀없음.
+
 from collections import Counter
 import sys
 
@@ -7,7 +10,7 @@ number_counter = Counter(number_list)
 stack, result = [], [-1] * n
 
 for i, value in enumerate(number_list):
-    while stack and number_counter[stack[-1][1]] < number_counter[value]:
+    while stack and number_counter[stack[-1][1]] < number_counter[value]:       # 달라진 부분
         _i, _value = stack.pop()
         result[_i] = value
     stack.append([i,  value])
